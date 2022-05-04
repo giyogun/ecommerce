1a468a1019749eaae7c74280a19d43edbb13a7e9
Godwin Iyogun <giyogun@gmail.com>
2022-05-04 18:52:15 +0100

    connect to sanity

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { client, urlFor } from "../lib/client";
+import FooterBanner from "../components/FooterBanner";
+import HeroBanner from "../components/HeroBanner";
+import Product from "../components/Product";
 
-const index = () => {
+const Home = ({ products, banner }) => {
   return (
-    <div>index</div>
-  )
-}
+    <>
+      <HeroBanner heroBanner={banner.length && banner[0]} />
+      {console.log(products)}
+      {console.log(banner)}
+      <div className="products-heading">
+        <h2>Best Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div className="products-container">
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
+      </div>
+      <FooterBanner footerBanner={banner && banner[0]} />
+    </>
+  );
+};
 
-export default index
+export const getServerSideProps = async () => {
+  const products = await client.fetch(`*[_type == "product"]`);
+  const banner = await client.fetch(`*[_type == "banner"]`);
+
+  return {
+    props: { products, banner },
+  };
+};
+
+export default Home;
